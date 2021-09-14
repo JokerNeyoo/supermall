ea98c47cab1198701cd586ab019e5ebf97d0aeef
JokerNeyoo <654752045@qq.com>
2021-09-14 23:46:04 +0800

    目录结构

---
+++ src/components/content/mainTabbar/MainTabBar.vue
@@ -0,0 +1,57 @@
+<template>
+  <tab-bar>
+    <tab-bar-item path="/home" activeColor="blue">
+      <img src="~assets/imgs/tabbar/home.svg" alt="" slot="item-icon" />
+      <img
+        src="~assets/imgs/tabbar/home_active.svg"
+        alt=""
+        slot="item-active-icon"
+      />
+      <div slot="item-text">首页</div>
+    </tab-bar-item>
+    <tab-bar-item path="/category" activeColor="blue">
+      <img src="~assets/imgs/tabbar/category.svg" alt="" slot="item-icon" />
+      <img
+        src="~assets/imgs/tabbar/category_active.svg"
+        alt=""
+        slot="item-active-icon"
+      />
+      <div slot="item-text">分类</div>
+    </tab-bar-item>
+    <tab-bar-item path="/cart" activeColor="blue">
+      <img src="~assets/imgs/tabbar/shopcart.svg" alt="" slot="item-icon" />
+      <img
+        src="~assets/imgs/tabbar/shopcart_active.svg"
+        alt=""
+        slot="item-active-icon"
+      />
+
+      <div slot="item-text">购物车</div>
+    </tab-bar-item>
+    <tab-bar-item path="/profile" activeColor="blue">
+      <img src="~assets/imgs/tabbar/profile.svg" alt="" slot="item-icon" />
+      <img
+        src="~assets/imgs/tabbar/profile_active.svg"
+        alt=""
+        slot="item-active-icon"
+      />
+
+      <div slot="item-text">我的</div>
+    </tab-bar-item>
+  </tab-bar>
+</template>
+
+<script>
+import TabBar from "common/tabbar/TabBar.vue";
+import TabBarItem from "common/tabbar/TabBarItem.vue";
+
+export default {
+  name: "MainTabBar",
+  components: {
+    TabBar,
+    TabBarItem,
+  },
+};
+</script>
+<style scoped>
+</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
-import { createApp } from 'vue'
+// import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
 
-createApp(App).mount('#app')
+Vue.config.productionTip = false
+new Vue({
+    render: h => h(App),
+    router
+}).$mount('#app')
